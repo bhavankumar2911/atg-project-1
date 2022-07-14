1a3f216f1e8133f809a90aa304ddff164a2d89fa
Bhavan Kumar <bhavankumarcse2020@gmail.com>
2022-07-14 18:45:27 +0530

    posts filter section made responsive

--- src/components/PostsFilter/index.jsx
+++ src/components/PostsFilter/index.jsx
@@ -1,21 +1,52 @@
 import React from "react";
 import Arrow from "./assets/arrow.png";
+import AddPeople from "./assets/add-people.png";
 import "./styles.css";
 
 function PostsFilter() {
   return (
-    <section
-      id="posts-filter"
-      className="d-flex justify-content-between align-items-center"
-    >
-      <span>Posts(368)</span>
-      <span className="bg-primary">
-        <button className="btn btn-light btn-sm">
-          Filter:All{" "}
-          <img src={Arrow} alt="down arrow" id="posts-filter-arrow" />
-        </button>
-      </span>
-    </section>
+    <>
+      <section
+        id="posts-filter"
+        className="d-flex justify-content-between align-items-center d-lg-none"
+      >
+        <span>Posts(368)</span>
+        <span className="bg-primary">
+          <button className="btn btn-light btn-sm">
+            Filter:All{" "}
+            <img src={Arrow} alt="down arrow" id="posts-filter-arrow" />
+          </button>
+        </span>
+      </section>
+      <section className="d-none d-lg-block">
+        <div className="row g-0" id="posts-filter-lg">
+          <div className="col-6 offset-1">
+            <ul id="category-list" className="d-flex">
+              <li>All Posts(32)</li>
+              <li>Article</li>
+              <li>Event</li>
+              <li>Education</li>
+              <li>Job</li>
+            </ul>
+            <hr />
+          </div>
+          <div className="col-4" id="posts-filter-lg-cta">
+            <div className="d-flex justify-content-end">
+              <button
+                className="btn btn-light btn-sm"
+                style={{ backgroundColor: "#EDEEF0", marginRight: "16px" }}
+              >
+                Write a Post <img src={Arrow} alt="down arrow symbol" />
+              </button>
+              <button className="btn btn-primary btn-sm">
+                <img src={AddPeople} alt="join group symbol" /> Join Group
+              </button>
+            </div>
+            <hr />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
